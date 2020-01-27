e4e41b176839f981aacdacf87b0538dce6eae292
Vadim <yurkinvadim99@gmail.com>
2020-01-28 01:06:26 +0300

    update
    update

--- index/views.py
+++ index/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import *
 
-# Create your views here.
+
 def index(request):
     cards = Card.objects.all()
     return render(request, 'index/index.html', context={'cards': cards})
